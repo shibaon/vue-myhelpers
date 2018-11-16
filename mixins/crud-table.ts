2070a201ab145f2fb9763fb18d63351d38e9d135
Valery <shibaon@gmail.com>
2018-11-16 19:24:10 +0300

    Update and rename crud-table.js to crud-table.ts

--- mixins/crud-table.ts
+++ mixins/crud-table.ts
@@ -4,9 +4,6 @@ export default {
             cols: [['id', '#'], ['name', 'Add columns!!!']],
             count: 0,
             limit: 10,
-            page: 0,
-            filter: {},
-            order: ['id', 'desc'],
             page: this.$route.query.page ? parseInt(this.$route.query.page, 10) : 0,
             filter: this.$route.query.filter ? JSON.parse(this.$route.query.filter) : {},
             order: [this.$route.query.ob ? this.$route.query.ob : 'id', this.$route.query.od ? this.$route.query.od : 'desc'],
@@ -29,5 +26,5 @@ export default {
         updateQuery() {
             this.$router.push({ path: this.$route.path, query: { ob: this.order[0], od: this.order[1], page: this.page, filter: JSON.stringify(this.filter) } })
         },
-    }
+    },
 }
