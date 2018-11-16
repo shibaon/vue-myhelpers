db94e505f40b0f2e6ccdce5c51393ebf511ee7ea
Valery <shibaon@gmail.com>
2018-11-16 19:49:45 +0300

    Update crud-table.ts

--- mixins/crud-table.ts
+++ mixins/crud-table.ts
@@ -1,40 +1,29 @@
-export const CrudTable = {
-    data() {
-        return {
-            cols: [['id', '#'], ['name', 'Add columns!!!']],
-            count: 0,
-            limit: 10,
-            page: this.$route.query.page ? parseInt(this.$route.query.page, 10) : 0,
-            filter: this.$route.query.filter ? JSON.parse(this.$route.query.filter) : {},
-            order: [this.$route.query.ob ? this.$route.query.ob : 'id', this.$route.query.od ? this.$route.query.od : 'desc'],
-        }
-    },
-    computed: {
-        pages() {
-            return Math.ceil(this.count / this.limit)
-        },
-    },
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
     watch: {
         order: 'updateQuery',
         page: 'updateQuery',
         filter: 'updateQuery',
     },
-    created() {
+})
+export default class extends Vue {
+    protected cols = [['id', '#'], ['name', 'Add columns!!!']]
+    protected count = 0
+    protected limit = 10
+    protected page = this.$route.query.page ? parseInt(this.$route.query.page, 10) : 0
+    protected filter = this.$route.query.filter ? JSON.parse(this.$route.query.filter) : {},
+    protected order = [this.$route.query.ob ? this.$route.query.ob : 'id', this.$route.query.od ? this.$route.query.od : 'desc']
+
+    get pages() {
+            return Math.ceil(this.count / this.limit)
+    }
+
+    public created() {
         this.updateQuery()
-    },
-    methods: {
-        updateQuery() {
-            this.$router.push({ path: this.$route.path, query: { ob: this.order[0], od: this.order[1], page: this.page, filter: JSON.stringify(this.filter) } })
-        },
-    },
-}
-export default CrudTable
-export interface ICrudTable {
-    cols: Array<[string, string]>
-    count: number
-    limit: number
-    page: number
-    filter: { [key: string]: string }
-    order: [string, 'asc'|'desc']
-    updateQuery: () => void
+    }
+
+    public updateQuery() {
+        this.$router.push({ path: this.$route.path, query: { ob: this.order[0], od: this.order[1], page: this.page, filter: JSON.stringify(this.filter) } } as any)
+    }
 }
